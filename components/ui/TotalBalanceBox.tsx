36c55415fdff213c0c2308f0aeb21865ec5bc129
Shady-Prakash <shadyprakash8@gmail.com>
2025-01-15 01:34:13 +0000

    WIP on dashboar headerbox

---
+++ components/ui/TotalBalanceBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TotalBalanceBox = ({
+  accounts =[], totalBanks, totalCurrentBalancet
+}: TotlaBalanceBoxProps) => {
+  return (
+    <section className="total-balance">TotalBalanceBox</section>
+  )
+}
+
+export default TotalBalanceBox
